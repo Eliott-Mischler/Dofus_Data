b573ad6f862a0a2e4e73275d7387f97ea8ca3fdc
Eliott Mischler <emischler2001@gmail.com>
2022-10-24 08:05:21 +0200

    added runes

--- date.py
+++ date.py
@@ -5,7 +5,9 @@ class Datetime(Base):
     __tablename__ = 'date_times'
 
     id = Column(Integer, primary_key = True)
+    type = Column(String)
     date_time = Column(DateTime)
 
-    def __init__(self, date_time):
+    def __init__(self, date_time, type):
         self.date_time = date_time,
+        self.type = type
--- run.py
+++ run.py
@@ -6,10 +6,10 @@ from base import Session, Base, engine
 Base.metadata.create_all(engine)
 
 
-def log_prices(name_list, price_list):
+def log_prices(name_list, price_list, type):
     session = Session()
     now = datetime.now()
-    now = Datetime(now)
+    now = Datetime(now, type)
     for name, price in zip(name_list, price_list):
         item = Item(name, price)
         item.date_time = now
